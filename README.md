bf89e153c7ed8b6bce53b93bea9574d1b28231ca
majiandong <836240219@qq.com>
2017-10-01 15:57:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Sublime-Transit
+一个为解决在 Sublime 编辑器上直接翻译单词的插件。使用[有道智云](http://ai.youdao.com/docs/doc-trans-api.s#p02)提供的翻译 api 实现。
+
---
+++ transit.py
@@ -0,0 +1,64 @@
+import sublime
+import sublime_plugin
+import hashlib
+import urllib
+import random
+import threading
+import json
+
+
+class TransitCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        sels = self.view.sel()
+        for sel in sels:
+            words = self.view.substr(sel)
+            # print(words)
+            thread = YouDaoApiCall(words, 5)
+            thread.start()
+
+
+class YouDaoApiCall(threading.Thread):
+
+    def __init__(self, string, timeout):
+        self.original = string
+        self.timeout = timeout
+        threading.Thread.__init__(self)
+
+    def run(self):
+        appKey = '075aa28fd372d61b'
+        secretKey = 'dse5SgFXbSNfkq3PrQhzeSlH0birnloZ'
+        myurl = 'https://openapi.youdao.com/api'
+        q = self.original
+        fromLang = 'auto'
+        toLang = 'auto'
+        salt = random.randint(1, 65536)
+
+        sign = appKey+q+str(salt)+secretKey
+        sign = hashlib.md5(sign.encode('utf-8')).hexdigest()
+        myurl = myurl+'?appKey='+appKey+'&q='+urllib.parse.quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
+
+        try:
+            conn = urllib.request.urlopen(myurl, timeout=self.timeout)
+            response = conn.read()
+            result = response.decode('utf-8')
+            self.parse(result)
+            return
+        except Exception as e:
+            sublime.error_message(str(e))
+        finally:
+            conn.close()
+
+    def parse(self, result):
+        jsonObj = json.loads(result)
+        try:
+            resArr = jsonObj['basic']['explains']
+        except Exception as e:
+            resArr = ['未找到释义']
+        finally:
+            sublime.active_window().show_quick_panel(resArr, self.on_select)
+
+    def on_select(self, index):
+        # if index > -1:
+        #     sublime.message_dialog(str(index))
+        pass
