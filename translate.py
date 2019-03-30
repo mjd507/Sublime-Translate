5963cf6fe95fcf8fc5b53170118873641c02984d
tjc0726 <49085201+tjc0726@users.noreply.github.com>
2019-03-30 11:44:32 +0800

    Add files via upload

--- translate.py
+++ translate.py
@@ -6,15 +6,16 @@ import urllib
 import random
 import threading
 import json
-# requests lib is more easier to use.
-import requests
 import bs4
 from bs4 import BeautifulSoup
-
+import sys
+import uuid
+import requests
+import hashlib
+import time
 
 def get_setting(key, defVal):
     return sublime.load_settings("Translate-CN.sublime-settings").get(key, defVal)
-
 
 class TranslateTextCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -26,7 +27,7 @@ class TranslateTextCommand(sublime_plugin.TextCommand):
                 words = self.view.substr(wholeWord)
             else:
                 words = self.view.substr(sel)
-            # print(words)
+            #print(words)
             if words != '':
                 if get_setting("ciba", False):
                     thread = CibaApiCall(words)
@@ -55,7 +56,6 @@ class TranslateInputCommand(sublime_plugin.TextCommand):
 
 
 class YouDaoApiCall(threading.Thread):
-
     def __init__(self, words):
         self.words = words
         self.appKey = get_setting("appKey", "")
@@ -64,40 +64,64 @@ class YouDaoApiCall(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        appKey = self.appKey
-        secretKey = self.secretKey
-        myurl = 'https://openapi.youdao.com/api'
+        data = {}
+
+        def truncate(q):
+            if q is None:
+                return None
+            size = len(q)
+            return q if size <= 20 else q[0:10] + str(size) + q[size - 10:size]
+
+        def do_request(data):
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            return requests.post(YOUDAO_URL, data=data, headers=headers)
+
+        def encrypt(signStr):
+            hash_algorithm = hashlib.sha256()
+            hash_algorithm.update(signStr.encode('utf-8'))
+            return hash_algorithm.hexdigest()
+        
+        YOUDAO_URL = 'http://openapi.youdao.com/api'
+        APP_KEY = self.appKey
+        APP_SECRET = self.secretKey
         q = self.words
-        fromLang = 'auto'
-        toLang = 'auto'
-        salt = random.randint(1, 65536)
-
-        sign = appKey + q + str(salt) + secretKey
-        sign = hashlib.md5(sign.encode('utf-8')).hexdigest()
-        myurl = myurl + '?appKey=' + appKey + '&q=' + urllib.parse.quote(
-            q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
-
+        data['from'] = 'auto'
+        data['to'] = 'auto'
+        data['signType'] = 'v3'
+        curtime = str(int(time.time()))
+        data['curtime'] = curtime
+        salt = str(uuid.uuid1())
+        signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET
+        sign = encrypt(signStr)
+        data['appKey'] = self.appKey
+        secretKey = self.secretKey
+        data['q'] = q
+        data['salt'] = salt
+        data['sign'] = sign
+        sign = hashlib.sha256(sign.encode('utf-8')).hexdigest()
         try:
-            conn = urllib.request.urlopen(myurl, timeout=self.timeout)
-            response = conn.read()
-            result = response.decode('utf-8')
-            # print(result)
-            self.parse(result)
+            response = do_request(data)
+            result = response.content.decode('utf-8')
+            if q.endswith(('.','?','!',',')):
+                self.parse(result,0)
+            else:
+                self.parse(result,1)
             return
         except Exception as e:
             sublime.error_message(str(e))
-        finally:
-            conn.close()
 
-    def parse(self, result):
+    def parse(self, result,flag):
         jsonObj = json.loads(result)
         try:
-            resArr = jsonObj['basic']['explains']
+            if flag == 1:
+                resArr = jsonObj['basic']['explains']
+            else:
+                resArr = jsonObj['translation']
         except Exception as e:
             resArr = ['/(ㄒoㄒ)/~~ 未找到释义']
         finally:
             self.resArr = resArr
-            sublime.active_window().show_quick_panel(resArr, self.on_select)
+            sublime.active_window().show_quick_panel(resArr,self.on_select)
 
     def on_select(self, index):
         if index > -1:
@@ -134,7 +158,7 @@ class CibaApiCall(threading.Thread):
     def parse(self, result):
         # print(result)
         if not result:
-            translate_final = ['/(ㄒoㄒ)/~~ 未找到释义']
+            translate_final = ['/(ㄒoㄒ)/~~ 未找到ciba释义']
         else:
             # 不知道为什么在subl里ul的class就没有了。
             # temp_results = result[0].find_all('ul', class_='base-list')
@@ -142,7 +166,7 @@ class CibaApiCall(threading.Thread):
             temp_results = result[0].find_all('span', class_='prop')[0].parent.parent
             # print(temp_results)
             if not temp_results:
-                translate_final = ['/(ㄒoㄒ)/~~ 未找到释义']
+                translate_final = ['/(ㄒoㄒ)/~~ 未找到ciba释义']
             else:
                 # print(temp_results)
                 translate_final = []
@@ -154,7 +178,7 @@ class CibaApiCall(threading.Thread):
                                 temp_str += x.text
                         translate_final.append(node.span.text + " " + temp_str)
         if not translate_final:
-            translate_final = ['/(ㄒoㄒ)/~~ 未找到释义']
+            translate_final = ['/(ㄒoㄒ)/~~ 未找到ciba释义']
         self.resArr = translate_final
         sublime.active_window().show_quick_panel(translate_final, self.on_select)
 
